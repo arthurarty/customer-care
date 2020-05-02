504b11636191014bdcd4d6ca2b85966507567589
arthurarty <arthur.nangai@gmail.com>
2020-05-02 23:43:30 +0300

    chore: Use wtforms.

---
+++ customer_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class CustomerForm(FlaskForm):
+    """Customer form"""
+
+    customer_name = StringField('Customer Name', validators=[DataRequired()])
+    product = StringField('Product you interested in', validators=[DataRequired()])
+    phone_number = StringField('Phone Number', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
+from customer_form import CustomerForm
+
 
 load_dotenv()
 
@@ -13,23 +15,23 @@ app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
+app.secret_key = os.getenv('SECRET_KEY')
 mail = Mail(app)
 
 @app.route('/')
 def index():
-    return render_template('form.html')
+    return render_template('form.html', form=CustomerForm())
 
 @app.route('/submit', methods = ['POST'])
 def handle_form():
-    name = request.form['customer_name']
-    phone_number = request.form['phone_number']
-    product = request.form['product']
-    email = request.form['email']
-    body = f"Hello, {name} just ordered {product} reach him on {phone_number} or at {email}"
-    msg = Message("New order placed", sender="arthur.nangai@gmail.com", recipients=["martinkatamba@gmail.com"])
-    msg.body = body
-    mail.send(msg)
-    return body
+    form = CustomerForm()
+    if form.validate_on_submit():
+        body = f"Hello, {form.customer_name.data} ordered {form.product.data} no: {form.phone_number.data} email: {form.email.data}"
+        msg = Message("New order placed", sender="arthur.nangai@gmail.com", recipients=["art@gmail.com"])
+        msg.body = body
+        mail.send(msg)
+        return "Email sent"
+    return "there has been an eror"
 
 if __name__ == '__main__':
     app.run(debug=True)
